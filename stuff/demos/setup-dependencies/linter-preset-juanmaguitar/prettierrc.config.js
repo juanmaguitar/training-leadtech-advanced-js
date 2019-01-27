594968ab95defa63dc5db8d68df4cdb9cdfb7809
JuanMa Garrido <juanma.garrido@gmail.com>
2019-01-27 11:56:39 +0100

    setup-dependencies & starter-project-dependencies

---
+++ stuff/demos/setup-dependencies/linter-preset-juanmaguitar/prettierrc.config.js
@@ -0,0 +1,13 @@
+// https://prettier.io/docs/en/configuration.html
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: false,
+  semi: false,
+  useTabs: false,
+  parser: 'babylon',
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid'
+}
