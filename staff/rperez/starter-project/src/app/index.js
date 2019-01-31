e02080dd4b35f587ca96fe19ce5e451b4fed2c3c
rperez1990 <rperez@leadtech.com>
2019-01-31 17:30:32 +0100

    Closure functions exercises

--- staff/rperez/starter-project/src/app/index.js
+++ staff/rperez/starter-project/src/app/index.js
@@ -10,3 +10,24 @@ class List {
 
 const list = new List(2, 3, 4)
 list.add(2)
+
+function howManyRequests () {
+  var requests = 0
+  return function () {
+    return ++requests
+  }
+}
+
+var requestsDone = howManyRequests()
+requestsDone()
+requestsDone()
+
+function password (password) {
+  return function () {
+    return password
+  }
+}
+
+var passwordRead = password('123456')
+passwordRead()
+passwordRead()
