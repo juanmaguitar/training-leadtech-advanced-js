c1c03f7acbee8f2ee448d9e73b8718d1a15ded08
Alfonso Villegas López <avillegas@leadtech.com>
2019-01-31 16:43:13 +0100

    first commit

---
+++ staff/avillegas/starter-project/src/app/index.js
@@ -0,0 +1,20 @@
+console.log('hey!')
+
+const name = 'Alfonso'
+const age = 25
+
+const me = { name, age }
+console.log(me)
+
+const city = 'barcelona'
+const alsoMe = { ...me, city }
+
+console.log(alsoMe)
+
+const a = {}
+
+if (a === 2) {
+  console.log('desd')
+} else {
+  console.log('asdad')
+}
