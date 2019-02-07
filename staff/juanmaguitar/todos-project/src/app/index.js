0e5463b496ae7343f95c05c80d194fa6ede9c62f
JuanMa Garrido <juanma.garrido@gmail.com>
2019-02-07 17:03:49 +0100

    axios get promises

--- staff/juanmaguitar/todos-project/src/app/index.js
+++ staff/juanmaguitar/todos-project/src/app/index.js
@@ -1,1 +1,18 @@
-console.log('hey!')
+import axios from 'axios'
+
+const url = 'https://jsonplaceholder.typicode.com/todos'
+
+const getDataFromResponse = ({ data }) => data
+const getDelayedObjectWithTime = data =>
+  new Promise(resolve => {
+    const time = Math.floor(Math.random() * 5000)
+    setTimeout(() => {
+      resolve({ data, time })
+    }, time)
+  })
+
+axios
+  .get(url)
+  .then(getDataFromResponse)
+  .then(getDelayedObjectWithTime)
+  .then(console.log)
