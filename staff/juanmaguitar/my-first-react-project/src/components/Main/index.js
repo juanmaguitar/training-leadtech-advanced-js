e3bb8b231554490b9d724d9bea4ef38bde440f4d
JuanMa Garrido <juanma.garrido@gmail.com>
2019-02-11 18:51:59 +0100

    counter

--- staff/juanmaguitar/my-first-react-project/src/components/Main/index.js
+++ staff/juanmaguitar/my-first-react-project/src/components/Main/index.js
@@ -1,9 +1,12 @@
 import React from "react"
 import './index.css'
 
+import Counter from '../Counter/'
+
 const Main = () => (
   <div className="Main">
     <h1>Main</h1>
+    <Counter />
   </div>
 )
 
