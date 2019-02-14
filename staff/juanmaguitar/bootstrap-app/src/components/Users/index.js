7f9873d2ae15c9e7a09559a12740698c1adf3c85
JuanMa Garrido <juanma.garrido@gmail.com>
2019-02-14 18:28:32 +0100

    cards list

--- staff/juanmaguitar/bootstrap-app/src/components/Users/index.js
+++ staff/juanmaguitar/bootstrap-app/src/components/Users/index.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import Card from '../Card/'
 
+import './index.scss'
 
-const Users = props => {
-  console.log(props)
+const BASE_CLASS = 'Users'
+const CLASS_LIST_CARDS = `${BASE_CLASS}-listCards`
+
+const Users = ({users}) => {
+  const usersList = Object.values(users)
   return (
-    <h1>Users</h1>
+    <div className={BASE_CLASS}>
+      <h2>{usersList.length} users</h2>
+      <div className={CLASS_LIST_CARDS}>
+        {
+          usersList
+            .map(user => <Card key={user.user_id} {...user}/>)
+        }
+      </div>
+    </div>
   )
 }
 
