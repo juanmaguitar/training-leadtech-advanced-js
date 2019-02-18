84934be38c23fcdf768f54aa74af9d0d81c7e0d6
Miguel Fdez <mfernandez@leadtech.com>
2019-02-18 14:09:43 +0100

    React lesson with bootstrap

--- staff/mfernandez/bootstrap-app/src/components/App/index.js
+++ staff/mfernandez/bootstrap-app/src/components/App/index.js
@@ -1,22 +1,60 @@
-import React, { Component } from "react";
-import { Container } from "react-bootstrap";
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./index.css";
 
-import Navbar from "../Navbar/";
-import Home from "../Home/";
-import Users from "../Users/";
+import { Container } from 'react-bootstrap';
+
+import Navbar from '../Navbar/'
+import Home from '../Home/'
+import Users from '../Users/'
+
+import {contacts} from '../../helpers/static-data'
+console.log(contacts)
 
 class App extends Component {
+  state = {
+    selected: []
+  }
+
+  addSelected = id => {
+    this.setState({
+      selected: [...this.state.selected, id]
+    }) 
+  }
+
+  deleteSelected = id => {
+    this.setState({
+      selected: this.state.selected.filter(userId => userId !== id)
+    }) 
+  }
+
+  toggleSelected = id => {
+    if (this.state.selected.includes(id)) {
+      this.deleteSelected(id)
+    } else {
+      this.addSelected(id)
+    }
+  }
+  
   render() {
+    const {selected} = this.state
     return (
       <Router>
         <Container>
-          <Navbar />
+          <Navbar selected={selected}/>
+          <h1>App</h1>
           <Route exact path="/" component={Home} />
-          <Route path="/users" component={Users} />
+          <Route path="/users" render={props => {
+            return (
+              <Users 
+                {...props} 
+                users={contacts} 
+                selected={selected}
+                toggleSelected={this.toggleSelected}
+              />
+            )
+          }} />
         </Container>
-      </Router>
+      </Router>  
     );
   }
 }
